884b22cf1c40ab4bcd1abe584b995672c3e93d63
Lilian Liu <lilian.kun.liu@gmail.com>
2021-06-05 11:46:13 +1000

    add more things

--- 1 lubridate.R
+++ 1 lubridate.R
@@ -33,13 +33,19 @@ date()
 
 today()
 
-month(today())
+#month function 
+#(x, label = FALSE, abbr = TRUE, locale = Sys.getlocale("LC_TIME")) UseMethod("month")
 
+
+month(x = today(),label = T)
+#[1] Jun
+#12 Levels: Jan < Feb < Mar < Apr < May < Jun < Jul < Aug < Sep < ... < Dec
 
 
 d <- '2011-12-12'
 
 d2 <- ymd(d)
+# ymd() -- Transforms dates stored in character and numeric vectors to Date 
 
 class(d)
 #"character"
@@ -47,8 +53,10 @@ class(d)
 class(d2)
 #"Date"
 
-
+#system dates 
 Sys.Date()
 
-
+#Suspend execution of R expressions for a specified time interval.
 Sys.sleep(20)
+#sleep for 20 seconds
+
